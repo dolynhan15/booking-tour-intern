0f696da6862073beeb5c7811d0063b3f3fdc92f4
Do Ly Nhan <dolynhan15@gmail.com>
2021-12-14 22:30:48 +0700

    create model

--- booking_tour/api_category/models.py
+++ booking_tour/api_category/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from api_base.models import BaseModel
+
+
 # Create your models here.
 class Category(BaseModel):
     name = models.CharField(max_length=255, null=True)
+
     class Meta:
-        db_table="api_categorys"
+        db_table = "api_categories"
