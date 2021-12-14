d64296b2e2766267d84df986f81afe5bbb34100d
prd-tue-phan <tue.phan@paradox.ai>
2021-12-14 23:06:55 +0700

    update user

--- booking_tour/api_user/models/user.py
+++ booking_tour/api_user/models/user.py
@@ -11,39 +11,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     password = models.CharField(verbose_name="password", max_length=255)
     email = models.EmailField(max_length=255, unique=True, null=True)
     active = models.BooleanField(default=True)
-    staff = models.BooleanField(default=False)  # staff not superuser
-    admin = models.BooleanField(default=False)  # superuser
+    staff = models.BooleanField(default=False)  # staff is company admin
+    admin = models.BooleanField(default=False)  # admin is super admin
     timestamp = models.DateTimeField(auto_now_add=True)
-    USERNAME_FIELD = "email"  # username
 
     objects = UserManager()
 
     class Meta:
         db_table = "api_users"
 
-    def has_perm(self, perm, obj=None):
-        return True
-
-    def has_module_perms(self, app_label):
-        return True
-
-    @property
-    def is_staff(self):
-        return self.staff
-
-    @property
-    def is_active(self):
-        return self.active
-
-    @property
-    def is_admin(self):
-        return self.admin
-
     def set_password(self, raw_password):
         self.password = make_password(password=raw_password, salt=settings.SECRET_KEY)
         self._password = raw_password
-
-    @property
-    def user_profile(self):
-        # noinspection PyUnresolvedReferences
-        return UserProfile.objects.filter(id=self.profile.id)
